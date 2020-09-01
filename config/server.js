5e82e54a9cd32ffc29a7e0c91c84164511e6e656
苏震 <suzhen@viewhigh.com>
2020-09-01 23:56:53 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e31947e587c1b1e09cb266576a49854a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'defd482e-a695-499b-a777-a8a049f53aa9'
+};
